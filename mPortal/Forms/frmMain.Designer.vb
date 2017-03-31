600cb79ca19c6bd10c0eab6cd57d5965bd07fbe5
amoah <amoah@DESKTOP-1P2S3KT>
2017-03-31 17:02:55 +0000

    i added a buton

--- mPortal/Forms/frmMain.Designer.vb
+++ mPortal/Forms/frmMain.Designer.vb
@@ -22,17 +22,30 @@ Partial Class frmMain
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(307, 147)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'frmMain
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1096, 462)
+        Me.ClientSize = New System.Drawing.Size(822, 375)
+        Me.Controls.Add(Me.Button1)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "frmMain"
         Me.Text = "mPortal"
         Me.ResumeLayout(False)
 
     End Sub
 
+    Friend WithEvents Button1 As Button
 End Class
