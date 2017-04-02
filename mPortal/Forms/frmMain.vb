093021eed678796b79997c11a24e9cec18fc5aff
A4men <i4ittest@gmail.com>
2017-04-02 09:56:01 +0000

    Syn solved 2

--- mPortal/Forms/frmMain.vb
+++ mPortal/Forms/frmMain.vb
@@ -1,13 +1,13 @@
-﻿Public Class frmMain
-    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        MsgBox("Hello All")
-    End Sub
+﻿'Public Class frmMain
+'    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+'        MsgBox("Hello All")
+'    End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+'    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
-        'myConn.Open()
-        'MsgBox("oK")
-    End Sub
+myConn.Open()
+MsgBox("oK")
+End Sub
 
 
-End Class
+'End Class
