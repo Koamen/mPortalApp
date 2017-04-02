5d3dfad64993bb291bfe9040520f495a36984d59
A4men <i4ittest@gmail.com>
2017-04-02 09:51:04 +0000

    Conn problem solved

--- mPortal/Forms/frmMain.vb
+++ mPortal/Forms/frmMain.vb
@@ -5,8 +5,8 @@
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
-        myConn.Open()
-        MsgBox("oK")
+        'myConn.Open()
+        'MsgBox("oK")
     End Sub
 
 
